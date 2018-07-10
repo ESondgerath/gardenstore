dbe970a94bea056851918c941f9eb8abd6928811
Evan Sondgerath <esondgerath@gmail.com>
2018-07-10 15:57:59 -0400

    Initial Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
