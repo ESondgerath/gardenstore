af0b191dd4ebcb28622153c1ab637d6c03ecbaee
Evan Sondgerath <esondgerath@gmail.com>
2018-07-11 10:28:04 -0400

    NavbarFooter Fix Attempt 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { UiModule } from './ui/ui.module';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { LayoutComponent } from './ui/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
